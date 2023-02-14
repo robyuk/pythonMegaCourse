6aceffb31206db674de358681bbba1cd19d36b55
Robert <youngr2000-github@yahoo.com>
2023-02-14 10:25:23 +0000

    Python Mega Course Day 19 Updates Added streamlit examples: ex_streamlit.py greypic.py

--- bonus/greypic.py
+++ bonus/greypic.py
@@ -1,16 +1,27 @@
 """
 Starts the user's camera, allows the user to take a still picture,
-then reders a grey-scale image in the web page
+then renders a grey-scale image in the web page.
+Added an option to allow the user to pick a picture file and render that in grey-scale
 """
 import streamlit as st
 from PIL import Image
 
 GREY = "L"  # Image.convert argument for grey_scale
 
-with st.expander("Start Camera"):
-    pic = st.camera_input("Camera")  # Start the camera
 
-if pic:
+def do_pic(pic):
     img = Image.open(pic)  # Create a Pillow image instance
     grey = img.convert(GREY)  # Convert to greyscale
+    st.info("\nHere is your picture in grey-scale:")
     st.image(grey)  # render the image in the web page
+
+
+uploaded_pic = st.file_uploader("Upload Image")
+
+with st.expander("Start Camera"):
+    camera_pic = st.camera_input("Camera")  # Start the camera
+
+if uploaded_pic:
+    do_pic(uploaded_pic)
+if camera_pic:
+    do_pic(camera_pic)
